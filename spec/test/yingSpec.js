11f3bd69726411218eb1f2567eb08b77a44a2110
Mgen <m@mgenware.com>
2015-08-24 19:00:25 +0800

    Add more tests

---
+++ spec/test/yingSpec.js
@@ -0,0 +1,37 @@
+var ying = require('../../src/ying_main');
+
+describe('ying test', function() {
+    
+    var specData = [
+        {
+            src: '<p>{{name}}</p>', 
+            pas: [{name: 'Mgen >>>'}], 
+            exp: ['<p>Mgen &gt;&gt;&gt;</p>']
+        },
+        {
+            src: '<p>{{= _e(d.id ? d.id + "~" + d.name : d.error) }}</p>', 
+            pas: [{id: 123, name: 'Mgen >>>'}, {error: 'Fun with ying'}],
+            exp: ['<p>123~Mgen &gt;&gt;&gt;</p>', '<p>Fun with ying</p>']
+        },
+        {
+            src: '<ul>{{# var s = ""; for(var i in d.users) s += "<li>" + _e(d.users[i]) + "</li>"; return s; }}</ul>',
+            pas: [{users: ['aaa', 'b', 'cccc', 'dd']}],
+            exp: ['<ul><li>aaa</li><li>b</li><li>cccc</li><li>dd</li></ul>']
+        },
+        {
+            src: '<body>{{= d.content }}</body>',
+            pas: [{content: '<h1>hello</h1>'}],
+            exp: ['<body><h1>hello</h1></body>']
+        }
+    ];
+    
+    var i = 1;
+    specData.forEach(function (specItem) {
+        var func = ying.compile(specItem.src);
+        specItem.pas.forEach(function (pasItem, index) {
+            it('spec ' + i++, function () {
+                expect(func(pasItem)).toBe(specItem.exp[index]);
+            });
+        });
+    });
+});
